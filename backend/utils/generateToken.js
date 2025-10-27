ae8c9893ec46caa643275e7e52d1cab1aade1c1a
flameztech01 <flameztech01@gmail.com>
2025-10-27 17:25:59 +0100

    generate Token

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -5,10 +5,12 @@ const generateToken = (res, userId) => {
 
     res.cookie('jwt', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
-        sameSite: 'strict',
+        secure: process.env.NODE_ENV !== 'development' || true,
+        sameSite: 'None',
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     });
+
+    return token;
 };
 
 export default generateToken;
