a542f87e3f8cdd36b3308aeddc13561fe964bfa9
flameztech01 <flameztech01@gmail.com>
2025-10-27 17:14:58 +0100

    token

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,13 +1,21 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'; 
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
- baseUrl: `${import.meta.env.VITE_API_URL || ''}/api`,
+  baseUrl: `${import.meta.env.VITE_API_URL || ''}/api`,
   credentials: 'include',
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.userInfo?.token; // 👈 get token from redux
+
+    if (token) {
+      headers.set('Authorization', `Bearer ${token}`); // 👈 attach token
+    }
+
+    return headers;
+  },
 });
 
-
 export const apiSlice = createApi({
-    baseQuery,
-    tagTypes: ['User'],
-    endpoints: (builder) => ({}),
-})
+  baseQuery,
+  tagTypes: ['User', 'Blog'],
+  endpoints: (builder) => ({}),
+});
